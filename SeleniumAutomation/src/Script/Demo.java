29c35c564be01deb8a46c316218c8f9774636b45
Administrator <manoharvs8@gmail.com>
2019-02-22 20:55:56 +0530

    Framework code pushed

---
+++ SeleniumAutomation/src/Script/Demo.java
@@ -0,0 +1,35 @@
+package Script;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void  setup() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+	}
+	
+	@Test
+	
+	public void test()
+	{
+		driver.get("http://www.freecrm.com/");
+		System.out.println(driver.getTitle());
+	}
+
+	@AfterMethod
+	
+	public void teardown()
+	{
+		driver.close();
+	}
+}
